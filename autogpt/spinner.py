9bb5bc90b126154be5f35d4fbe85722df8bc8dfe
LAPTOP-G43MA5MO\ekarni <eyalk5@gmail.com>
2023-05-06 08:15:08 +0300

    Added interrupt to commands

--- autogpt/spinner.py
+++ autogpt/spinner.py
@@ -1,8 +1,30 @@
 """A simple spinner module"""
+import asyncio
 import itertools
+import select
 import sys
+import termios
 import threading
 import time
+import tty
+
+
+class SpinnerInterrupted(Exception):
+    pass
+
+
+class RaisingThread(threading.Thread):
+    def run(self):
+        self._exc = None
+        try:
+            super().run()
+        except Exception as e:
+            self._exc = e
+
+    def join(self, timeout=None):
+        super().join(timeout=timeout)
+        if self._exc:
+            raise self._exc
 
 
 class Spinner:
@@ -13,6 +35,8 @@ class Spinner:
         message: str = "Loading...",
         delay: float = 0.1,
         plain_output: bool = False,
+        interruptable: bool = False,
+        interrupt_action=None,
     ) -> None:
         """Initialize the spinner class
 
@@ -27,6 +51,9 @@ class Spinner:
         self.message = message
         self.running = False
         self.spinner_thread = None
+        self.interruptable = interruptable
+        self.interrupt_action = interrupt_action
+        self.ended = threading.Event()
 
     def spin(self) -> None:
         """Spin the spinner"""
@@ -35,6 +62,20 @@ class Spinner:
             return
         while self.running:
             self.print_message()
+            # Add non-blocking reading of char to stop spinner
+            if (
+                self.interruptable
+                and sys.stdin in select.select([sys.stdin], [], [], 0.0)[0]
+            ):
+                key = sys.stdin.read(1)
+                if key == " ":
+                    if self.interrupt_action is not None:
+                        self.interrupt_action()
+
+                elif key == "q":
+                    self.ended.set()
+                    raise SpinnerInterrupted("Spinner interrupted")
+
             time.sleep(self.delay)
 
     def print_message(self):
@@ -45,7 +86,11 @@ class Spinner:
     def __enter__(self):
         """Start the spinner"""
         self.running = True
-        self.spinner_thread = threading.Thread(target=self.spin)
+        self.oldtty = termios.tcgetattr(sys.stdin)
+        self.stdin_no = sys.stdin.fileno()
+        tty.setcbreak(self.stdin_no)
+
+        self.spinner_thread = RaisingThread(target=self.spin)
         self.spinner_thread.start()
 
         return self
@@ -59,8 +104,14 @@ class Spinner:
             exc_traceback (Exception): The exception traceback.
         """
         self.running = False
-        if self.spinner_thread is not None:
-            self.spinner_thread.join()
+        try:
+            if self.spinner_thread is not None:
+                self.spinner_thread.join()
+        finally:
+            while sys.stdin in select.select([sys.stdin], [], [], 0.0)[0]:
+                sys.stdin.read(1)
+            termios.tcsetattr(self.stdin_no, termios.TCSADRAIN, self.oldtty)
+
         sys.stdout.write(f"\r{' ' * (len(self.message) + 2)}\r")
         sys.stdout.flush()
 
